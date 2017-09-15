108eeaa764d5c576169a40e81a695e364bf831f2
arvin <caojinliang@fosun.com>
2017-09-15 14:37:29 +0800

    server

--- Server/index.php
+++ Server/index.php
@@ -1,9 +1,9 @@
 <?php
 
-$serv = new swoole_server('127.0.0.0', 9503, SWOOLE_BASE, SWOOLE_SOCK_TCP);
+$serv = new swoole_server('127.0.0.1', 9501, SWOOLE_BASE, SWOOLE_SOCK_TCP);
 $serv->set([
     'worker_num' => 4,
-    'daemonize'  => true,
+    //'daemonize'  => true,
     'backlog'    => 128
 ]);
 
@@ -15,15 +15,15 @@ $serv->start();
 
 function onConnect($serv, $fd)
 {
-    echo '连接上了';
+    echo "连接上了\n";
 }
 
 function onReceive($serv, $fd, $from_id, $data)
 {
-    echo '收到了';
+    echo "收到了\n";
 }
 
 function onClose($serv, $fd)
 {
-    echo '关闭了';
+    echo "关闭了\n";
 }
